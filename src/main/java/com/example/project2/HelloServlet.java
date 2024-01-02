eb4fae489948a83d76de80627b32d85f47f596e6
ZanaSiaka <zanacoul21@gmail.com>
2024-01-02 19:35:15 +0000

    Debut du code Back-end

---
+++ src/main/java/com/example/project2/HelloServlet.java
@@ -0,0 +1,11 @@
+package com.example.project2;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+}
---
+++ src/main/java/com/example/project2/WebServlet.java
@@ -0,0 +1,7 @@
+package com.example.project2;
+
+public @interface WebServlet {
+    String name();
+
+    String value();
+}
